237bdb83095e86c2df23e64962341879fc626696
Kirill Puzanov <kiryjm@gmail.com>
2024-03-15 05:57:57 +0400

    refactor: add extension method to add app services

---
+++ students-courses-api/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Students.Courses.Api.Interfaces;
+using Students.Courses.Api.Services;
+using Students.Courses.Repository.Context;
+using Students.Courses.Services.Core;
+using Students.Courses.Services.Students;
+
+namespace Students.Courses.Api.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        services.AddTransient<IStudentService, StudentService>();
+        services.AddTransient<ICourseService, CourseService>();
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(StudentsList.Handler).Assembly));
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+        return services;
+    }
+}
--- students-courses-api/Program.cs
+++ students-courses-api/Program.cs
@@ -1,11 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Students.Courses.Api.Configuration;
-using Students.Courses.Api.Interfaces;
-using Students.Courses.Api.Services;
-using Students.Courses.Repository.Context;
-using Students.Courses.Repository.Seeds;
-using Students.Courses.Services.Core;
-using Students.Courses.Services.Students;
+using Students.Courses.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -13,28 +7,8 @@ var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(config);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy
-            .AllowAnyHeader()
-            .AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddTransient<IStudentService, StudentService>();
-builder.Services.AddTransient<ICourseService, CourseService>();
-builder.Services.AddMediatR(cfg =>
-    cfg.RegisterServicesFromAssembly(typeof(StudentsList.Handler).Assembly));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
